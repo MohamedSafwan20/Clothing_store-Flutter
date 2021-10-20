3ab4fc2ae0c4c3994751ce52fcc9228a67b79666
Mohamed Safwan <Mohamedsafwan220@gmail.com>
2021-10-20 13:59:10 +0530

    Added validation to product data in my orders page

--- lib/widgets/pending_orders.dart
+++ lib/widgets/pending_orders.dart
@@ -15,122 +15,149 @@ class PendingOrders extends StatefulWidget {
 class _PendingOrdersState extends State<PendingOrders> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        width: double.infinity,
-        child: ListView.builder(
-            itemCount: widget.productData.length,
-            itemBuilder: (context, index) {
-              return FutureBuilder(
-                  future: DbService.getRefData(
-                      widget.productData[index]["product_id"]),
-                  builder: (context, AsyncSnapshot snapshot) {
-                    if (snapshot.connectionState == ConnectionState.done) {
-                      if (snapshot.hasData) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 10.0,
-                            vertical: 15.0,
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              InkWell(
-                                onTap: () {
-                                  Navigator.pushNamed(context, "/order-details",
-                                      arguments: {
-                                        "productData": widget.productData[index]
-                                      });
-                                },
-                                child: SizedBox(
-                                  child: Column(
-                                    children: [
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
+    return widget.productData.isNotEmpty
+        ? SizedBox(
+            width: double.infinity,
+            child: ListView.builder(
+                itemCount: widget.productData.length,
+                itemBuilder: (context, index) {
+                  return FutureBuilder(
+                      future: DbService.getRefData(
+                          widget.productData[index]["product_id"]),
+                      builder: (context, AsyncSnapshot snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          if (snapshot.hasData) {
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 10.0,
+                                vertical: 15.0,
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  InkWell(
+                                    onTap: () {
+                                      Navigator.pushNamed(
+                                          context, "/order-details",
+                                          arguments: {
+                                            "productData":
+                                                widget.productData[index]
+                                          });
+                                    },
+                                    child: SizedBox(
+                                      child: Column(
                                         children: [
                                           Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
                                             crossAxisAlignment:
                                                 CrossAxisAlignment.start,
                                             children: [
-                                              Image.network(
-                                                snapshot.data["product_images"]
-                                                    [0],
-                                                fit: BoxFit.contain,
-                                                width: 135,
-                                              ),
-                                              Padding(
-                                                padding:
-                                                    const EdgeInsets.symmetric(
+                                              Row(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  Image.network(
+                                                    snapshot.data[
+                                                        "product_images"][0],
+                                                    fit: BoxFit.contain,
+                                                    width: 135,
+                                                  ),
+                                                  Padding(
+                                                    padding: const EdgeInsets
+                                                            .symmetric(
                                                         horizontal: 8.0),
-                                                child: Column(
-                                                  crossAxisAlignment:
-                                                      CrossAxisAlignment.start,
-                                                  children: [
-                                                    Padding(
-                                                      padding:
-                                                          const EdgeInsets.only(
-                                                              bottom: 4.0),
-                                                      child: Text(
-                                                          snapshot.data[
-                                                              "product_name"],
-                                                          style: TextStyle(
-                                                              fontWeight:
-                                                                  FontWeight
-                                                                      .bold,
-                                                              fontSize: 18,
-                                                              color: Theme.of(
-                                                                      context)
-                                                                  .colorScheme
-                                                                  .primary)),
+                                                    child: Column(
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .start,
+                                                      children: [
+                                                        Padding(
+                                                          padding:
+                                                              const EdgeInsets
+                                                                      .only(
+                                                                  bottom: 4.0),
+                                                          child: Text(
+                                                              snapshot.data[
+                                                                  "product_name"],
+                                                              style: TextStyle(
+                                                                  fontWeight:
+                                                                      FontWeight
+                                                                          .bold,
+                                                                  fontSize: 18,
+                                                                  color: Theme.of(
+                                                                          context)
+                                                                      .colorScheme
+                                                                      .primary)),
+                                                        ),
+                                                        Text(
+                                                            "₹${snapshot.data['product_price']}",
+                                                            style: TextStyle(
+                                                                color: Theme.of(
+                                                                        context)
+                                                                    .colorScheme
+                                                                    .secondary,
+                                                                fontSize: 14)),
+                                                        Padding(
+                                                            padding:
+                                                                const EdgeInsets
+                                                                        .only(
+                                                                    top: 12.0),
+                                                            child: Text(
+                                                              widget.productData[
+                                                                      index]
+                                                                  ["status"],
+                                                              style: TextStyle(
+                                                                  color: Theme.of(
+                                                                          context)
+                                                                      .indicatorColor,
+                                                                  fontSize: 20,
+                                                                  fontWeight:
+                                                                      FontWeight
+                                                                          .w700),
+                                                            ))
+                                                      ],
                                                     ),
-                                                    Text(
-                                                        "₹${snapshot.data['product_price']}",
-                                                        style: TextStyle(
-                                                            color: Theme.of(
-                                                                    context)
-                                                                .colorScheme
-                                                                .secondary,
-                                                            fontSize: 14)),
-                                                    Padding(
-                                                        padding:
-                                                            const EdgeInsets
-                                                                    .only(
-                                                                top: 12.0),
-                                                        child: Text(
-                                                          widget.productData[
-                                                              index]["status"],
-                                                          style: TextStyle(
-                                                              color: Theme.of(
-                                                                      context)
-                                                                  .indicatorColor,
-                                                              fontSize: 20,
-                                                              fontWeight:
-                                                                  FontWeight
-                                                                      .w700),
-                                                        ))
-                                                  ],
-                                                ),
+                                                  ),
+                                                ],
                                               ),
                                             ],
                                           ),
                                         ],
                                       ),
-                                    ],
+                                    ),
                                   ),
-                                ),
+                                ],
                               ),
-                            ],
-                          ),
-                        );
-                      } else {
-                        return const Error();
-                      }
-                    } else {
-                      return const Loading();
-                    }
-                  });
-            }));
+                            );
+                          } else {
+                            return const Error();
+                          }
+                        } else {
+                          return const Loading();
+                        }
+                      });
+                }))
+        : Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  "https://cdn-icons-png.flaticon.com/512/1103/1103140.png",
+                  width: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    "You didn't have any orders.",
+                    style: TextStyle(
+                        color: Theme.of(context).disabledColor,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+          );
   }
 }
